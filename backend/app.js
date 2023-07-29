c1cd8f51f19070e0b47d89dd183fe14c16971fcf
Rashidul Islam <rashidulkd@gmail.com>
2023-07-29 22:22:20 +0600

    cors issues solved

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'https://ecommerce-app-e7bu.vercel.app/',
+  origin: 'https://ecommerce-app-e7bu.vercel.app',
   credentials: true
 }));
 
